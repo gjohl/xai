4ebc3933e3283dbf60b90cb5a41870af244815cd
gjohl <gurpreetjohl@gmail.com>
2023-11-13 23:04:28 +0000

    Add simplex distance class

--- xai/models/simple_cnn.py
+++ xai/models/simple_cnn.py
@@ -8,7 +8,7 @@ from simplexai.models.base import BlackBox
 class CNNClassifier(BlackBox):
     def __init__(self) -> None:
         """CNN binary classifier model"""
-        super(CNNClassifier, self).__init__()
+        super().__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
