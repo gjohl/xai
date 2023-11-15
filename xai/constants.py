681a9a939328a0d86abbd96448536c817f4b54ef
Gurpreet Johl <gurpreetjohl@gmail.com>
2023-11-15 23:43:52 +0000

    PHD-70 Tweak simplex distance in experiment (#11)
    * Script tweaks
    
    * Tweak simplex distance
    
    * Script improvements

--- xai/constants.py
+++ xai/constants.py
@@ -6,3 +6,4 @@ import xai
 BASE_DIR = Path(xai.__file__).parents[1]
 DATA_DIR = BASE_DIR / 'xai' / 'data'
 MODEL_DIR = BASE_DIR / 'xai' / 'models' / 'saved_models'
+RESULTS_DIR = BASE_DIR / 'xai' / 'experiments' / 'results'
--- xai/experiments/mnist_binary_classification/evaluate_model_performance.py
+++ xai/experiments/mnist_binary_classification/evaluate_model_performance.py
@@ -1,6 +1,9 @@
+import pickle
+
+from simplexai.explainers.simplex import Simplex
 import torch
 
-from xai.constants import MODEL_DIR
+from xai.constants import MODEL_DIR, RESULTS_DIR
 from xai.data_handlers.utils import load_training_data_mnist_binary, load_test_data_mnist_binary
 from xai.evaluation_metrics.distance import SimplexDistance, LatentPointwiseDistance, LatentApproxDistance
 from xai.evaluation_metrics.performance import calculate_accuracy_metrics
@@ -8,12 +11,32 @@ from xai.models.simple_cnn import CNNBinaryClassifier
 
 
 MODEL_FNAME = 'binary_cnn_mnist_run_1.pth'
-BATCH_SIZE = 1024
+BATCH_SIZE = 64
+
+
+def run_and_save_results(output_fname, digits, num_samples):
+    output_fpath = RESULTS_DIR / output_fname
+    metrics_dict = run_multiple(digits, num_samples)
+
+    with open(output_fpath, 'wb') as handle:
+        pickle.dump(metrics_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    return metrics_dict
 
 
 def run_multiple(digits, num_samples):
     model = load_binary_classification_model(MODEL_FNAME)
-    train_dl, _ = load_training_data_mnist_binary(batch_size=BATCH_SIZE, shuffle=False, train_validation_split=[1., 0.])
+    train_dl, validation_dl = load_training_data_mnist_binary(batch_size=BATCH_SIZE,
+                                                              shuffle=False,
+                                                              train_validation_split=[0.8, 0.2])
+    source_data, _ = next(iter(train_dl))
+    validation_data, _ = next(iter(validation_dl))
+
+    # Fit simplex on the validation data
+    source_latents = model.latent_representation(source_data).detach()
+    validation_latents = model.latent_representation(validation_data).detach()
+    simplex = Simplex(corpus_examples=source_data, corpus_latent_reps=source_latents)
+    simplex.fit(test_examples=validation_data, test_latent_reps=validation_latents, reg_factor=0, n_epoch=10000)
 
     out_of_dist_pct_range = [k/10 for k in range(11)]
     metrics_dict = {}
@@ -23,9 +46,10 @@ def run_multiple(digits, num_samples):
         count_per_digit = get_count_per_digit(digits, num_samples, out_of_dist_pct)
         test_dl = load_test_data_mnist_binary(batch_size=BATCH_SIZE, shuffle=True,
                                               digits=digits, count_per_digit=count_per_digit)
+        target_data, _ = next(iter(test_dl))
 
         accuracy_metrics = model_accuracy_metrics(model, test_dl)
-        distance_metrics = model_distance_metrics(model, train_dl, test_dl)
+        distance_metrics = model_distance_metrics(model, source_data, target_data, simplex)
         results_dict = accuracy_metrics | distance_metrics  # Merge dicts into single result
         metrics_dict[out_of_dist_pct] = results_dict
 
@@ -45,12 +69,9 @@ def model_accuracy_metrics(model, test_dl):
     return metrics
 
 
-def model_distance_metrics(model, train_dl, test_dl):
-    source_data, _ = next(iter(train_dl))
-    target_data, _ = next(iter(test_dl))
-
+def model_distance_metrics(model, source_data, target_data, simplex):
     distance_dict = {}
-    simplex_dist = SimplexDistance(model, source_data, target_data)
+    simplex_dist = SimplexDistance(model, source_data, target_data, simplex)
     latent_pw_dist = LatentPointwiseDistance(model, source_data, target_data)
     latent_approx_dist = LatentApproxDistance(model, source_data, target_data)
 
@@ -92,22 +113,3 @@ def get_count_per_digit(digits, num_samples_per_class, out_of_dist_pct):
         count_per_digit[digit] = int(out_of_dist_num_samples / len(out_of_dist_digits))
 
     return count_per_digit
-
-
-# TODO GJ: remove
-# digits = (0, 1, 6)
-# num_samples = 30
-
-# import pandas as pd
-# metrics_dict = run_multiple((0, 1, 6), 300)
-# df = pd.DataFrame(metrics_dict).T
-#
-# import pickle
-# output_fpath = '/home/gurp/workspace/xai/xai/experiments/results/mnist_extrapolation_016_300.pkl'
-#
-# with open(output_fpath, 'wb') as handle:
-#     pickle.dump(metrics_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
-#
-# df[['accuracy', 'simplex']]
-#
-# from matplotlib import pyplot as plt
