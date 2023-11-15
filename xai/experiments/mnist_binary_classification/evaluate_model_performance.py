790ceec3fa59c52dfe68a12a3a312ea9b93c8207
gjohl <gurpreetjohl@gmail.com>
2023-11-15 21:14:16 +0000

    Run multiple model performance script

--- xai/experiments/mnist_binary_classification/evaluate_model_performance.py
+++ xai/experiments/mnist_binary_classification/evaluate_model_performance.py
@@ -7,41 +7,20 @@ from xai.models.simple_cnn import CNNBinaryClassifier
 
 
 MODEL_FNAME = 'binary_cnn_mnist_run_1.pth'
-DIGITS = (0, 1, 2)
-NUM_SAMPLES_PER_CLASS = 1000
 
 
-def run_multiple():
+def run_multiple(digits, num_samples):
     model = load_binary_classification_model(MODEL_FNAME)
 
     out_of_dist_pct_range = [k/10 for k in range(11)]
     metrics_dict = {}
     for idx, out_of_dist_pct in enumerate(out_of_dist_pct_range):
         print(f"Running metrics for {idx+1} of {len(out_of_dist_pct_range)}")
-        out_of_dist_num_samples = int(NUM_SAMPLES_PER_CLASS*out_of_dist_pct)
-        count_per_digit = {
-            0: NUM_SAMPLES_PER_CLASS - out_of_dist_num_samples,
-            1: NUM_SAMPLES_PER_CLASS,
-            2: out_of_dist_num_samples,
-        }
-        metrics = evaluate_model_metrics(model, DIGITS, count_per_digit)
+        count_per_digit = get_count_per_digit(digits, num_samples, out_of_dist_pct)
+        metrics = evaluate_model_metrics(model, digits, count_per_digit)
         metrics_dict[out_of_dist_pct] = metrics
 
-
-def get_count_per_digit(digits, num_samples_per_class, out_of_dist_pct):
-    # The count for 0 and 1 are determined completely by num_samples_per_class and out_of_dist_pct
-    out_of_dist_num_samples = num_samples_per_class * out_of_dist_pct
-    count_per_digit = {
-        0: num_samples_per_class - int(out_of_dist_num_samples),
-        1: num_samples_per_class
-    }
-
-    # The count of out of dist digits depends on how many there are
-    out_of_dist_digits = set(digits).difference({0, 1})
-    for digit in out_of_dist_digits:
-        count_per_digit[digit] = int(out_of_dist_num_samples / len(out_of_dist_digits))
-
-    return count_per_digit
+    return metrics_dict
 
 
 def load_binary_classification_model(model_filename=MODEL_FNAME):
@@ -72,3 +51,24 @@ def calculate_model_predictions(model, test_dl):
     predicted_classes = (output_probs_all > 0.5) * 1
 
     return output_probs_all, predicted_classes, test_labels_all
+
+
+def get_count_per_digit(digits, num_samples_per_class, out_of_dist_pct):
+    # The count for 0 and 1 are determined completely by num_samples_per_class and out_of_dist_pct
+    out_of_dist_num_samples = num_samples_per_class * out_of_dist_pct
+    count_per_digit = {
+        0: num_samples_per_class - int(out_of_dist_num_samples),
+        1: num_samples_per_class
+    }
+
+    # The count of out of dist digits depends on how many there are
+    out_of_dist_digits = set(digits).difference({0, 1})
+    for digit in out_of_dist_digits:
+        count_per_digit[digit] = int(out_of_dist_num_samples / len(out_of_dist_digits))
+
+    return count_per_digit
+
+
+# TODO GJ: remove
+# metrics_dict = run_multiple((0, 1, 6), 30)
+# {k: v['auc'] for k,v in metrics_dict.items()}
