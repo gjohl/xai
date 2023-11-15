a839d9e4763635dc6c441eea09e7f6706fa59cd4
Gurpreet Johl <gurpreetjohl@gmail.com>
2023-11-15 14:53:10 +0000

    PHD-61 Binary class datasets (#6)
    * Add load_mnist_binary function and tests
    
    * Delete unused notebook
    
    * flake8

--- xai/data_handlers/mnist.py
+++ xai/data_handlers/mnist.py
@@ -1,5 +1,9 @@
+import functools
+import operator
+from typing import List, Dict
+
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 import torchvision
 
 from xai.constants import DATA_DIR
@@ -15,7 +19,7 @@ def load_mnist(
     shuffle: bool = True,
     data_dir=DATA_DIR
 ) -> DataLoader:
-    """
+    """Load MNIST data.
 
     Parameters
     ----------
@@ -57,3 +61,94 @@ def load_mnist(
             dataset, torch.randperm(len(dataset))[:subset_size]
         )
     return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+
+
+def load_mnist_binary(
+    batch_size: int,
+    train: bool,
+    target_digit: int,
+    subset_size=None,
+    shuffle: bool = True,
+    data_dir=DATA_DIR,
+    digits: List[int] = None,
+    count_per_digit: Dict[int, int] = None
+) -> DataLoader:
+    """Load MNIST data reframed for a binary classification task of distinguishing 1s.
+
+    Parameters
+    ----------
+    batch_size: int
+        Number of samples per batch to load.
+    train: bool
+        Whether to create the dataset using the training image set `train-images-idx3-ubyte` or
+        the test set `t10k-images-idx3-ubyte`.
+    subset_size: int, optional
+        If set, this extracts a subset of a given size rather than the full data set.
+        Defaults to None, which loads the entire data set.
+    shuffle: bool, optional
+        Whether to reshuffle the data at every epoch.
+    data_dir: path-like, optional
+        The directory to download files to.
+        This defaults to the `data` subdirectory of the xai project.
+
+    Returns
+    -------
+    DataLoader
+        A DataLoader containing MNIST image data.
+    """
+    dataset = torchvision.datasets.MNIST(
+        data_dir,
+        train=train,
+        download=True,
+        transform=torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize(*DEFAULT_MNIST_NORMALIZATION),
+        ]),
+        target_transform=lambda x: x == target_digit
+    )
+
+    if digits:
+        dataset = dataset_subset_by_digit(dataset, digits, count_per_digit=count_per_digit)
+
+    if subset_size:
+        dataset = torch.utils.data.Subset(
+            dataset, torch.randperm(len(dataset))[:subset_size]
+        )
+    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+
+
+def dataset_subset_by_digit(dataset: Dataset, digits: List[int], count_per_digit: Dict[int, int] = None):
+    """Select subset containing specific digits.
+
+    Parameters
+    ----------
+    dataset: Dataset
+        The Dataset from which we want to extract a subset.
+    digits: list of int
+        Digits to include from the original dataset.
+        If None, include all digits.
+    count_per_digit: dict
+        Keys are the digits, values are the number of instances of that digit that we want.
+        If None, include all instances.
+
+    Returns
+    -------
+    Dataset
+        The required subset of the Dataset.
+    """
+    digit_mask_list = []
+    for digit in digits:
+        digit_mask = dataset.targets == digit
+        if count_per_digit:
+            # If the user has specified a count n for the digit, only include the first n instances.
+            running_count = torch.cumsum(digit_mask, 0)
+            required_digit_count = count_per_digit[digit]
+            running_count_mask = running_count <= required_digit_count
+            digit_mask &= running_count_mask
+        digit_mask_list.append(digit_mask)
+
+    # Logical or of all masks, i.e. the union of given digits
+    digits_union_mask = functools.reduce(operator.or_, digit_mask_list)
+    digits_idx = digits_union_mask.nonzero().squeeze()
+
+    return torch.utils.data.Subset(dataset, digits_idx)
