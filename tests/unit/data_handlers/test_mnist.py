f2829b389d712d2eafbd085311ca37e3773ea3c8
gjohl <gurpreetjohl@gmail.com>
2023-11-15 14:49:38 +0000

    Add load_mnist_binary function and tests

---
+++ tests/unit/data_handlers/test_mnist.py
@@ -0,0 +1,68 @@
+import pytest
+from torch.utils.data import DataLoader
+import torchvision
+
+from xai.constants import DATA_DIR
+from xai.data_handlers.mnist import load_mnist_binary, dataset_subset_by_digit, DEFAULT_MNIST_NORMALIZATION
+
+
+def test_load_mnist_binary():
+    batch_size = 4
+    count_per_digit = {0: 8, 1: 8}
+    dl = load_mnist_binary(
+        batch_size=batch_size,
+        train=True,
+        target_digit=1,
+        subset_size=None,
+        shuffle=False,
+        data_dir=DATA_DIR,
+        digits=(0, 1),
+        count_per_digit=count_per_digit
+    )
+    dl_inputs, dl_labels = next(iter(dl))
+
+    assert len(dl) == sum(count_per_digit.values()) // batch_size
+    assert dl_inputs.shape[0] == batch_size
+    assert dl_labels.shape[0] == batch_size
+
+
+class TestDatasetSubSetByDigit:
+
+    @pytest.mark.parametrize('digits', [(0,), (0, 1,), (0, 1, 2,), (5, 6, 7)])
+    def test_subset_of_digits(self, digits):
+        dataset = torchvision.datasets.MNIST(
+            DATA_DIR,
+            train=True,
+            download=False,
+            transform=torchvision.transforms.Compose([
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize(*DEFAULT_MNIST_NORMALIZATION),
+            ]),
+        )
+        subset = dataset_subset_by_digit(dataset, digits)
+        dl = DataLoader(subset, batch_size=16)
+        dl_inputs, dl_labels = next(iter(dl))
+        assert all([label in digits for label in dl_labels])
+
+    @pytest.mark.parametrize('digits, count_per_digit', [
+        [(0,), {0: 10}],
+        [(0, 1), {0: 10, 1: 20}],
+        [(0, 1, 2,), {0: 10, 1: 20, 2: 10}],
+    ])
+    def test_count_per_digit(self, digits, count_per_digit):
+        dataset = torchvision.datasets.MNIST(
+            DATA_DIR,
+            train=True,
+            download=False,
+            transform=torchvision.transforms.Compose([
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize(*DEFAULT_MNIST_NORMALIZATION),
+            ]),
+        )
+        subset = dataset_subset_by_digit(dataset, digits, count_per_digit)
+        dl = DataLoader(subset, batch_size=40)
+        dl_inputs, dl_labels = next(iter(dl))
+        assert all([label in digits for label in dl_labels])
+        assert all([int(sum(dl_labels == digit)) == count_per_digit[digit] for digit in count_per_digit])
+
+
