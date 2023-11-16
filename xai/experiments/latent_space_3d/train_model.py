70105f50ac9398cd7603b820c95b7cb5e0bc2ce9
gjohl <gurpreetjohl@gmail.com>
2023-11-16 16:53:24 +0000

    Add models for 3d latent space

---
+++ xai/experiments/latent_space_3d/train_model.py
@@ -0,0 +1,7 @@
+from xai.models.simple_cnn import CNNBinaryClassifier3D
+from xai.experiments.model_training.train_binary_classifier import run_model_training
+
+
+model = CNNBinaryClassifier3D()
+model_output_filename = "binary_cnn_mnist_3d_run_1.pth"
+run_model_training(model, model_output_filename)
