c0878a0a4b5fe040bec51280e9fcbf395a34a5e0
gjohl <gurpreetjohl@gmail.com>
2023-11-19 14:53:52 +0000

    Add classwise_probability accuracy metrics

--- xai/evaluation_metrics/distance/__init__.py
+++ xai/evaluation_metrics/distance/__init__.py
@@ -1,5 +1,4 @@
 from .latent_distance import LatentPointwiseDistance, LatentApproxDistance
 from .simplex_distance import SimplexDistance
-from ..utils import DEFAULT_NORM
 
 __all__ = ['SimplexDistance', 'LatentPointwiseDistance', 'LatentApproxDistance']
