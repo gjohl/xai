d1b870e8f93fc8a0d3cbda3c33944b5d66147cd4
gjohl <gurpreetjohl@gmail.com>
2023-11-20 21:28:25 +0000

    Refactor experiment plots

--- xai/experiments/cancer_classification/collate_experiment_results.py
+++ xai/experiments/cancer_classification/collate_experiment_results.py
@@ -1,78 +1,15 @@
-import os
-import pickle
-
 from matplotlib import pyplot as plt
-import pandas as pd
 
 from xai.constants import RESULTS_DIR
-from xai.experiments.mnist_binary_classification.plot_utils import plot_ood_results_with_error_bars
+from xai.experiments.mnist_binary_classification.plot_utils import (
+    plot_ood_results_with_error_bars, collate_experiment_results
+)
 
 
 CANCER_RESULTS_DIR = RESULTS_DIR / 'cancer_experiment'
-RESULTS_COLS = [
-    'accuracy',
-    'f1',
-    'auc',
-    'probability_mean',
-    'probability_std',
-    'r_norm',
-    'r_vectorwise_norm',
-    'r_norm_classwise',
-    'r_norm_zeros',
-    'r_norm_ones',
-    'r_vectorwise_norm_classwise',
-    'r_vectorwise_norm_zeros',
-    'r_vectorwise_norm_ones',
-    'r_norm_directionwise',
-    'r_norm_direction_zeros',
-    'r_norm_direction_ones',
-    'r_vector_norm_directionwise',
-    'r_vector_norm_direction_zeros',
-    'r_vector_norm_direction_ones',
-    'h_norm_ratio',
-    'h_true_norm',
-    'h_approx_norm',
-    'h_norm_classwise',
-    'h_norm_zeros_ratio',
-    'h_norm_ones_ratio',
-    'h_norm_directionwise',
-    'h_norm_direction_zeros_ratio',
-    'h_norm_direction_ones_ratio',
-    'validation_h_norm_ratio',
-    'validation_h_true_norm',
-    'validation_h_approx_norm',
-    'validation_h_norm_classwise',
-    'validation_h_norm_zeros_ratio',
-    'validation_h_norm_ones_ratio',
-    'validation_h_norm_directionwise',
-    'validation_h_norm_direction_zeros_ratio',
-    'validation_h_norm_direction_ones_ratio'
-]
 
 
-#############
-# Load data #
-#############
-results_df_list = []
-for result_fname in os.listdir(CANCER_RESULTS_DIR):
-    with open(os.path.join(CANCER_RESULTS_DIR, result_fname), 'rb') as handle:
-        metrics_dict = pickle.load(handle)
-        results_df_list.append(pd.DataFrame(metrics_dict).T)
-
-
-####################################
-# Calc mean and std of each column #
-####################################
-df = results_df_list[0]
-
-mean_df = pd.DataFrame(index=df.index)
-std_df = pd.DataFrame(index=df.index)
-
-for col in RESULTS_COLS:
-    col_df = pd.concat([df[col] for df in results_df_list], axis=1)
-    mean_df[col] = col_df.mean(axis=1)
-    std_df[col] = col_df.std(axis=1)
-
+mean_df, std_df = collate_experiment_results(CANCER_RESULTS_DIR)
 
 ############################################
 # Plot columns of interest with error bars #
