a0c545a6b33262ae5597e5f304e835f208f44781
gjohl <gurpreetjohl@gmail.com>
2023-11-20 14:31:40 +0000

    Add error bar plots for results

--- xai/experiments/mnist_binary_classification/collate_experiment_results.py
+++ xai/experiments/mnist_binary_classification/collate_experiment_results.py
@@ -1,9 +1,11 @@
 import os
 import pickle
 
+from matplotlib import pyplot as plt
 import pandas as pd
 
 from xai.constants import RESULTS_DIR
+from xai.experiments.mnist_binary_classification.plot_utils import plot_ood_results_with_error_bars
 
 
 MNIST_RESULTS_DIR = RESULTS_DIR / 'mnist_experiment_2'
@@ -72,3 +74,12 @@ for col in RESULTS_COLS:
     std_df[col] = col_df.std(axis=1)
 
 
+############################################
+# Plot columns of interest with error bars #
+############################################
+fig = plot_ood_results_with_error_bars(mean_df, std_df, ['accuracy', 'auc', 'probability_mean'], "Classification measures")
+fig = plot_ood_results_with_error_bars(mean_df, std_df, ['probability_std'], "Probability standard deviation")
+fig = plot_ood_results_with_error_bars(mean_df, std_df, ['r_vectorwise_norm', 'r_vector_norm_directionwise'], "Residual norm")
+fig = plot_ood_results_with_error_bars(mean_df, std_df, ['h_true_norm', 'h_approx_norm'], "Latent space norm")
+fig = plot_ood_results_with_error_bars(mean_df, std_df, ['validation_h_norm_ratio', 'validation_h_norm_classwise'], "Relative latent space norm")
+fig = plot_ood_results_with_error_bars(mean_df, std_df, ['validation_h_norm_directionwise'], "Relative latent space norm in out-of-plave axis")
