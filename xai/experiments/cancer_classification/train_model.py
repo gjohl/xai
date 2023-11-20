654a806dd3604970bb4dea379fc7437eafef35e3
gjohl <gurpreetjohl@gmail.com>
2023-11-20 17:09:03 +0000

    Cancer CNN training script

---
+++ xai/experiments/cancer_classification/train_model.py
@@ -0,0 +1,30 @@
+from torch.utils.data import DataLoader
+
+from xai.constants import MODEL_DIR
+from xai.data_handlers.lung_colon_images import load_cancer_dataset
+from xai.models.cancer_classifier_cnn import CancerCNNClassifier
+from xai.models.training.learner import Learner
+
+BATCH_SIZE = 64
+
+
+# Load data and model
+model = CancerCNNClassifier()
+
+training_dataset = load_cancer_dataset('lung', 'train')
+validation_dataset = load_cancer_dataset('lung', 'validation')
+training_data_loader = DataLoader(training_dataset, batch_size=BATCH_SIZE, shuffle=True)
+validation_data_loader = DataLoader(validation_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+# Check model works on data
+source_data, source_labels = next(iter(training_data_loader))
+x = source_data[0]
+model.probabilities(x)
+
+# Train model
+learner = Learner(model, training_data_loader, validation_data_loader, num_epochs=10)
+learner.fit()
+
+# Save model
+model_output_filename = "cancer_cnn_run_1.pth"
+learner.save_model(MODEL_DIR / model_output_filename)
