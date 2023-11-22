b84e90860b004e3c9d0c6d197c44fb5f1363a608
Gurpreet Johl <gurpreetjohl@gmail.com>
2023-11-22 22:47:05 +0000

    PHD-29 Final report draft (#31)
    * Use tight_layout for figures
    
    * Regeerate latent 3D plots
    
    * Regenerate plot with new legends, axis labels
    
    * Results plots with tight_layout
    
    * Regerenate 2D latent plots
    
    * Revised report draft

--- xai/experiments/cancer_classification/collate_experiment_results.py
+++ xai/experiments/cancer_classification/collate_experiment_results.py
@@ -12,36 +12,38 @@ CANCER_FIGURES_DIR = FIGURES_DIR / 'cancer_experiment'
 
 mean_df, std_df = collate_experiment_results(CANCER_RESULTS_DIR)
 
+
 ############################################
 # Plot columns of interest with error bars #
 ############################################
-fig = plot_ood_results_with_error_bars(mean_df, std_df, ['accuracy', 'auc', 'probability_mean'],
-                                       "Classification metrics")
-plt.savefig(CANCER_FIGURES_DIR / 'classification_metrics.png', format='png')
+if __name__ == '__main__':
+    fig = plot_ood_results_with_error_bars(mean_df, std_df, ['accuracy', 'auc', 'probability_mean'],
+                                           "Classification metrics")
+    plt.savefig(CANCER_FIGURES_DIR / 'classification_metrics.png', format='png')
 
-fig = plot_ood_results_with_error_bars(mean_df, std_df, ['probability_std'],
-                                       "Probability standard deviation", legend=False)
-plt.savefig(CANCER_FIGURES_DIR / 'probability_std_dev.png', format='png')
+    fig = plot_ood_results_with_error_bars(mean_df, std_df, ['probability_std'],
+                                           "Probability standard deviation")
+    plt.savefig(CANCER_FIGURES_DIR / 'probability_std_dev.png', format='png')
 
-fig = plot_ood_results_with_error_bars(mean_df, std_df, ['r_vectorwise_norm'],
-                                       "Residual norm", legend=False)
-# fig = plot_ood_results_with_error_bars(mean_df, std_df, ['r_norm'], "Residual norm")
-plt.savefig(CANCER_FIGURES_DIR / 'residual_norm.png', format='png')
+    fig = plot_ood_results_with_error_bars(mean_df[['r_vectorwise_norm']], std_df[['r_vectorwise_norm']],
+                                           ['r_vectorwise_norm'], "Residual norm", renamed_cols=['r_norm'])
+    # fig = plot_ood_results_with_error_bars(mean_df, std_df, ['r_norm'], "Residual norm")
+    plt.savefig(CANCER_FIGURES_DIR / 'residual_norm.png', format='png')
 
-fig = plot_ood_results_with_error_bars(mean_df, std_df, ['h_true_norm', 'h_approx_norm'],
-                                       "Latent space norm")
-plt.savefig(CANCER_FIGURES_DIR / 'h_norm.png', format='png')
+    fig = plot_ood_results_with_error_bars(mean_df, std_df, ['h_true_norm', 'h_approx_norm'],
+                                           "Latent space norm")
+    plt.savefig(CANCER_FIGURES_DIR / 'h_norm.png', format='png')
 
-fig = plot_ood_results_with_error_bars(mean_df, std_df, ['validation_h_norm_ratio', 'validation_h_norm_classwise'],
-                                       "Relative latent space norm")
-plt.savefig(CANCER_FIGURES_DIR / 'relative_h_norm.png', format='png')
+    fig = plot_ood_results_with_error_bars(mean_df, std_df, ['validation_h_norm_ratio', 'validation_h_norm_classwise'],
+                                           "Relative latent space norm", renamed_cols=['rel_h_norm', 'rel_h_norm_classwise'])
+    plt.savefig(CANCER_FIGURES_DIR / 'relative_h_norm.png', format='png')
 
-fig = plot_ood_results_with_error_bars(mean_df, std_df, ['validation_h_norm_directionwise'],
-                                       "Relative latent space norm in out-of-plane axis", legend=False)
-plt.savefig(CANCER_FIGURES_DIR / 'relative_h_norm_directionwise.png', format='png')
+    fig = plot_ood_results_with_error_bars(mean_df, std_df, ['validation_h_norm_directionwise'],
+                                           "Relative latent space norm, out-of-plane", renamed_cols=['rel_h_norm_directionwise'])
+    plt.savefig(CANCER_FIGURES_DIR / 'relative_h_norm_directionwise.png', format='png')
 
 
-# Unused
-fig = plot_ood_results_with_error_bars(mean_df, std_df, ['r_norm_directionwise'], "Residual norm in out-of-plane axis")
-fig = plot_ood_results_with_error_bars(mean_df, std_df, ['r_vector_norm_directionwise'], "Residual norm in out-of-plane axis")
-fig = plot_ood_results_with_error_bars(mean_df, std_df, ['validation_h_norm_directionwise'], "Relative latent space norm in out-of-plane axis")
+    # Unused
+    fig = plot_ood_results_with_error_bars(mean_df, std_df, ['r_norm_directionwise'], "Residual norm in out-of-plane axis")
+    fig = plot_ood_results_with_error_bars(mean_df, std_df, ['r_vector_norm_directionwise'], "Residual norm in out-of-plane axis")
+    fig = plot_ood_results_with_error_bars(mean_df, std_df, ['validation_h_norm_directionwise'], "Relative latent space norm in out-of-plane axis")
